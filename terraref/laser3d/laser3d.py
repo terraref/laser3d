e821965c8a0afa156ef0630a1ad351995577a37b
Max Burnette <mburnet2@illinois.com>
2018-03-28 10:17:56 -0500

    add replacements for lasmerge, las2dem

--- terraref/laser3d/laser3d.py
+++ terraref/laser3d/laser3d.py
@@ -1,7 +1,7 @@
-import logging
 import subprocess
 import copy
 import math
+import numpy
 import laspy
 from plyfile import PlyData, PlyElement
 
@@ -14,27 +14,36 @@ def generate_las_from_ply(inp, out, pco):
     """
     if not isinstance(inp, list):
         inp = [inp]
+
+    # Create concatenated list of vertices to generate one merged LAS file
+    first = True
     for plyf in inp:
         plydata = PlyData.read(plyf)
-        for v in plydata['vertex']:
-            pts.append(((int(v[0]*100), int(v[1]*100), int(v[2]*100), 0, 9, 0, 0, 0, 0),))
+        merged_x = plydata['vertex']['x'] if first else numpy.concatenate([merged_x, plydata['vertex']['x']])
+        merged_y = plydata['vertex']['y'] if first else numpy.concatenate([merged_y, plydata['vertex']['y']])
+        merged_z = plydata['vertex']['z'] if first else numpy.concatenate([merged_z, plydata['vertex']['z']])
+        first = False
 
-    # add geo-reference offset (UTM ZONE 12) to the las header
-    h = laspy.header.Header()
-    h.x_offset = pco['x']*1000
-    h.y_offset = pco['y']*1000
-    h.z_offset = pco['z']*1000
+    # Create header and populate with scale and UTM-12 georeference offset
+    w = laspy.base.Writer(out, 'w', laspy.header.Header())
+    w.header.scale = [0.01, 0.01, 0.01]
+    w.set_header_property("x_offset", pco['x']*1000)
+    w.set_header_property("y_offset", pco['y']*1000)
+    w.set_header_property("z_offset", pco['z']*1000)
 
-    # Create LAS file without georegistration
-    f = laspy.file.File(out, h, mode='w')
-    f.points = pts
-    f.close()
+    # Add actual points from PLY files and update statistics to LAS file
+    w.set_x(merged_x, True)
+    w.set_y(merged_y, True)
+    w.set_z(merged_z, True)
+    w.header.update_min_max()
+    w.close()
 
-    # translate each point to there MAC coordinate without any modify in header
+    # Skip creation of f2 for now
+    return
+
+    # TODO: this replaces geo_referencing_las_for_eachpoint_in mac - is it still necessary?
     f = laspy.file.File(out, mode='r')
-    h2 = copy.copy(f.header)
-    h2.scale = [0.000001, 0.0001, 0.000001]
-    f2 = laspy.file.File(out.replace('.las', '_geo.las'), h2, mode='w')
+    f2 = laspy.file.File(out.replace('.las', '_geo.las'), f.header, mode='w')
     f2.X = f.X + long(math.floor(pco['x']*100000))
     f2.Y = f.Y + long(math.floor(pco['y']*100000))
     f2.Z = f.Z + long(math.floor(pco['z']*100000))
@@ -42,65 +51,11 @@ def generate_las_from_ply(inp, out, pco):
     f2.close()
 
 
-def combine_east_west_las(pdal_base, tmp_east_las, tmp_west_las, merge_las):
-    logging.getLogger(__name__).info("merging %s + %s into %s" % (tmp_east_las, tmp_west_las, merge_las))
-    subprocess.call([pdal_base+'pdal merge ' + \
-                     tmp_east_las+' '+tmp_west_las+' '+merge_las], shell=True)
-
-
-# This function add geo-reference offset(UTM ZONE 12) to the las header
-def geo_referencing_las(input_las_file, output_las_file, origin_coord):
-    #open las file
-    inFile = laspy.file.File(input_las_file, mode='r')
-
-    #get input header
-    input_header = inFile.header
-
-    # create output header
-    output_header = copy.copy(input_header)
-
-    output_header.x_offset = origin_coord['x']*1000
-    output_header.y_offset = origin_coord['y']*1000
-    output_header.z_offset = origin_coord['z']*1000
-
-    # save as new las file
-    output_file = laspy.file.File(output_las_file, mode='w', header=output_header)
-    output_file.points = inFile.points
-    output_file.close()
-
-    return
-
-# This function translate each point to there MAC coordinate without any modify in header
-def geo_referencing_las_for_eachpoint_in_mac(input_las_file, output_las_file, origin_coord):
-
-    # open las file
-    inFile = laspy.file.File(input_las_file, mode='r')
-
-    # output handle
-    new_header = copy.copy(inFile.header)
-    new_header.scale = [0.01,0.01,0.01]
-    output_file = laspy.file.File(output_las_file, mode='w', header=new_header)
-
-    output_file.X = inFile.X + long(math.floor(origin_coord['x']*100000))
-    output_file.Y = inFile.Y + long(math.floor(origin_coord['y']*100000))
-    output_file.Z = inFile.Z + long(math.floor(origin_coord['z']*100000))
-
-    output_file.close()
-
-def merge_las_by_name(input_list, output):
+def generate_tif_from_las(inp, out):
     """
-    input a list of las files location, the function will generate a merged las file to output directory
-    :param input_list: list of location of input files
-    :param output: string of output file location
-    :return:no return needed
+    :param inp: input LAS file
+    :param out: output GeoTIFF file
+    :param georef: boolean to perform rotation of GeoTIFF for proper location
     """
+    subprocess.call(['pklas2img -i '+inp+' -o '+out+' -comp max -n z -dx 2 -dy 2 -ot Float32'], shell=True)
 
-    basedire = os.path.join(os.path.dirname(__file__),"LAStools","bin","lasmerge")
-    inputform = ["-i "+s for s in input_list]
-    inputform = " ".join(inputform)
-    outputform = "-o " + output
-    sp_input = [basedire+" "+ inputform +" "+outputform]
-    FNULL = open(os.devnull, 'w')
-    subprocess.call(["make clean"],shell=True, stdout=FNULL,stderr=subprocess.STDOUT)
-    subprocess.call(["make"],shell=True,stdout=FNULL, stderr=subprocess.STDOUT)
-    subprocess.call(sp_input,shell=True,stdout=FNULL, stderr=subprocess.STDOUT)
