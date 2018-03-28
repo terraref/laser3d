e7963f9e0990ed78e6f86e85500fd2d6a671ccd5
Max Burnette <mburnet2@illinois.com>
2018-03-28 09:49:27 -0500

    remove LAStools and extractor files

--- terraref/laser3d/__init__.py
+++ terraref/laser3d/__init__.py
@@ -1,7 +1,5 @@
 __author__ = 'weiqin'
 
-from .ply2las import \
-    generate_las_from_pdal, geo_referencing_las, geo_referencing_las_for_eachpoint_in_mac, combine_east_west_las
-
-from .lasmerge import \
-    merge_las_by_name
+from .laser3d import \
+    generate_las_from_pdal, geo_referencing_las, geo_referencing_las_for_eachpoint_in_mac, \
+    combine_east_west_las, merge_las_by_name
