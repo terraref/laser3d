c9928ea2f8a8e683f6185bfed857ce143bec5c8c
Max Burnette <mburnet2@illinois.com>
2018-03-28 10:36:52 -0500

    fix init for pipit

--- terraref/laser3d/__init__.py
+++ terraref/laser3d/__init__.py
@@ -1,5 +1,4 @@
 __author__ = 'weiqin'
 
 from .laser3d import \
-    generate_las_from_pdal, geo_referencing_las, geo_referencing_las_for_eachpoint_in_mac, \
-    combine_east_west_las, merge_las_by_name
+    generate_las_from_ply, generate_tif_from_las, generate_slope_from_tif
