5ed65160a50d007c1a910f7e7eafc9b009d38e84
Max Burnette <mburnet2@illinois.com>
2018-03-28 12:58:31 -0500

    update setup file

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from setuptools import find_packages, setup
-import os
-
-
-datafiles = []
-for root, dirs, files in os.walk("terraref/laser3d/LAStools/"):
-      for file in files:
-            datafiles.append((root, [os.path.join(root, file)]))
 
 setup(name='terraref-laser3d',
       version='1.0.0',
       packages=find_packages(),
       namespace_packages=['terraref'],
       include_package_data=True,
-      url='https://github.com/terraref/ply2las',
-      data_files = datafiles
+      url='https://github.com/terraref/laser3d',
+      install_requires=[
+            'numpy',
+            'scipy',
+            'multiprocessing',
+            'matplotlib',
+            'Pillow',
+            'laspy',
+            'plyfile'
+      ]
       )
